87253451403b85f3ad5615e7d121f0321872d7d4
lzl19900514 <39326249+lzl19900514@users.noreply.github.com>
2023-01-07 15:27:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ DEFAULT_HEADERS = {
 }
 
 
-COOKIE = ''
+COOKIE = '__jsluid_h=28dff568a0e364743d469441d2701245; __ckguid=XCL5jU61nCwN633i5uO2833; device_id=21307064331663921772996867a53ff789dd2ee415b0ddeb25f1526dd2; homepage_sug=a; r_sort_type=score; __jsluid_s=56107b1384c3496a44c063159ee9c8e2; smzdm_user_source=7DF98205F9335EA52041279E6034B1D2; shequ_pc_sug=b; footer_floating_layer=0; ad_date=7; ad_json_feed=%7B%7D; _zdmA.vid=*; Hm_lvt_9b7ac3d38f30fe89ff0b8a0546904e58=1670555167,1671514400,1671590811,1673074073; sess=BA-1y4%2FIV%2BNnIpvjgbmPMS8u5k33FmOE1fG5g61geUuqVits%2Fbuub0p3%2Fa7zhPd8xr0eSHyw%2FNHBYkFSj9YWmBx%2BCYS%2BYWqp6gMLiU16OSLweycQukJegOvyetW; user=user%3A6298457852%7C6298457852; smzdm_id=6298457852; _zdmA.uid=ZDMA.sdIg_f4U0.1673075332.2419200; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%226298457852%22%2C%22first_id%22%3A%2218369776038da-0ebdff065b52f4-1a525635-1484784-183697760391408%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E5%BC%95%E8%8D%90%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC%22%2C%22%24latest_referrer%22%3A%22https%3A%2F%2Fgithub.com%2FChasing66%2Fsmzdm_bot%22%2C%22%24latest_landing_page%22%3A%22https%3A%2F%2Fwww.smzdm.com%2F%22%7D%2C%22%24device_id%22%3A%2218369776038da-0ebdff065b52f4-1a525635-1484784-183697760391408%22%7D; bannerCounter=%5B%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%5D; Hm_lpvt_9b7ac3d38f30fe89ff0b8a0546904e58=1673075333; _zdmA.time=1673075333140.0.https%3A%2F%2Fwww.smzdm.com%2F'
 
 
 PUSH_PLUS_TOKEN = ''
